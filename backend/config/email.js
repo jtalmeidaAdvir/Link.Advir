16e4ea65f0b5bb60fc97a02784c38ab7d78323d7
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-01-08 15:11:06 +0000

    update

--- backend/config/email.js
+++ backend/config/email.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'adviremailtestes@gmail.com', // O teu email
-        pass: 'hpfdapdpacvcdnks',          // A tua senha ou app-specific password
+        user: 'noreply.advir@gmail.com', // O teu email
+        pass: 'ihpgedswadmqtceh',          // A tua senha ou app-specific password
     },
 });
 
