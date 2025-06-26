b5c06a514e4f15ff6b5760ac748903d64479cf49
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-06-26 15:11:57 +0100

    commit

--- frontend/src/Servicos/PandIByTecnico.js
+++ frontend/src/Servicos/PandIByTecnico.js
@@ -20,6 +20,7 @@ import ChartsSection from './Components/PandIByTecnico/ChartsSection';
 import ModalDetalhesProcesso from './Components/PandIByTecnico/ModalDetalhesProcesso';
 import useProcessosPorDia from './Hooks/PandIByTecnico/useProcessosPorDia';
 import useProcessosFiltrados from './Hooks/PandIByTecnico/useProcessosFiltrados';
+import { getWeek, getWeeksInMonth, getDaysInWeek } from '../../Utils/dateUtils';
 
 
 
@@ -58,60 +59,10 @@ const MyDatePicker = ({ value, onChange, ...props }) => {
     }
 };
 
-// Obtém a semana do ano
-const getWeek = (date) => {
-    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-    const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
-    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
-};
 
-// Obtém as semanas de um mês específico
-const getWeeksInMonth = (month, year) => {
-    const weeks = [];
-    const firstDay = new Date(year, month - 1, 1);
-    const lastDay = new Date(year, month, 0);
 
-    let currentDate = new Date(firstDay);
 
-    // Ajustar para o primeiro dia da semana (domingo)
-    while (currentDate.getDay() !== 0) {
-        currentDate.setDate(currentDate.getDate() - 1);
-    }
 
-    // Iterar pelas semanas do mês
-    while (currentDate <= lastDay) {
-        const weekNumber = getWeek(currentDate);
-        if (!weeks.includes(weekNumber)) {
-            weeks.push(weekNumber);
-        }
-        currentDate.setDate(currentDate.getDate() + 7);
-    }
-
-    return weeks;
-};
-
-// Obtém os dias de uma semana específica
-const getDaysInWeek = (weekNumber, year) => {
-    const days = [];
-    const firstDayOfYear = new Date(year, 0, 1);
-
-    // Calcular o primeiro dia da semana
-    const firstDayOfWeek = new Date(firstDayOfYear);
-    firstDayOfWeek.setDate(
-        firstDayOfYear.getDate() +
-            (weekNumber - 1) * 7 -
-            firstDayOfYear.getDay(),
-    );
-
-    // Adicionar todos os dias da semana
-    for (let i = 0; i < 7; i++) {
-        const day = new Date(firstDayOfWeek);
-        day.setDate(firstDayOfWeek.getDate() + i);
-        days.push(day);
-    }
-
-    return days;
-};
 
 
 
