6d32c22cce8979aa21ad87c840b9ef9a5d5b44c7
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-08-12 15:01:41 +0100

    p

--- backend/models/contact.js
+++ backend/models/contact.js
@@ -1,32 +1,76 @@
 const { DataTypes } = require('sequelize');
+
 const { sequelize } = require('../config/db');
  
 const Contact = sequelize.define('Contact', {
+
     id: {
+
         type: DataTypes.INTEGER,
+
         primaryKey: true,
+
         autoIncrement: true
+
     },
+
     name: {
+
         type: DataTypes.STRING,
+
         allowNull: false
+
     },
+
     contacts: {
+
         type: DataTypes.TEXT,
+
         allowNull: false
+
     },
+
     can_create_tickets: {
+
         type: DataTypes.BOOLEAN,
+
         defaultValue: false,
+
         allowNull: false
+
     },
+
+    numero_tecnico: {
+
+        type: DataTypes.STRING,
+
+        allowNull: true
+
+    },
+
+    numero_cliente: {
+
+        type: DataTypes.STRING,
+
+        allowNull: true
+
+    },
+
     created_at: {
+
         type: DataTypes.DATE,
+
         defaultValue: DataTypes.NOW
+
     }
+
 }, {
+
     tableName: 'contacts',
+
     timestamps: false
+
 });
  
 module.exports = Contact;
+ 
