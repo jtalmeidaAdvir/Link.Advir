38c695e97417a385c2242bb126a1fe6064006029
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-08-28 17:09:50 +0100

    asd

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -25,7 +25,7 @@ const {
 } = require('../controllers/userController');
 const router = express.Router();
 
-const authMiddleware = require('../middleware/authMiddleware');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
 
 // Rotas de criação e autenticação de utilizadores
@@ -34,7 +34,7 @@ router.post('/criarUtilizadorAdmin', criarUtilizadorAdmin);
 router.post('/login', loginUtilizador);
 
 // Rota para alterar a password
-router.post('/alterarPassword', authMiddleware, alterarPassword);
+router.post('/alterarPassword', authenticateToken, alterarPassword);
 
 // Rotas de verificação de conta e recuperação de password
 router.get('/verify/:token', verificarConta);
@@ -44,34 +44,34 @@ router.post('/redefinir-password/:token', redefinirPassword);
 router.get('/getCodFuncionario/:userId', getCodFuncionario);
 
 // Rotas para operações de empresa
-router.get('/usersByEmpresa', authMiddleware, getUsersByEmpresa);
-router.get('/empresas', authMiddleware, getEmpresasByUser);
-router.post('/adicionar-empresa', authMiddleware, adicionarEmpresaAoUser);
-router.post('/remover-empresa', authMiddleware, removerEmpresaDoUser);
+router.get('/usersByEmpresa', authenticateToken, getUsersByEmpresa);
+router.get('/empresas', authenticateToken, getEmpresasByUser);
+router.post('/adicionar-empresa', authenticateToken, adicionarEmpresaAoUser);
+router.post('/remover-empresa', authenticateToken, removerEmpresaDoUser);
 
 // Rotas para módulos e submódulos
 router.get('/:userid/modulos', listarModulosDoUtilizador);
 router.get('/:userid/modulos-e-submodulos', listarModulosESubmodulosDoUtilizador);
 
 // Rota para fazer upload da imagem de perfil com autenticação
-router.post('/:userId/uploadProfileImage', authMiddleware, updateProfileImage);
+router.post('/:userId/uploadProfileImage', authenticateToken, updateProfileImage);
 
 // Corrija a rota para obter a imagem de perfil com o método GET
-router.get('/:userId/profileImage', authMiddleware, getProfileImage);
+router.get('/:userId/profileImage', authenticateToken, getProfileImage);
 
 // Rotas para empresa predefinida
-router.get('/:userId/empresa-predefinida', authMiddleware, obterEmpresaPredefinida);
-router.put('/:userId/empresa-predefinida', authMiddleware, definirEmpresaPredefinida);
+router.get('/:userId/empresa-predefinida', authenticateToken, obterEmpresaPredefinida);
+router.put('/:userId/empresa-predefinida', authenticateToken, definirEmpresaPredefinida);
 
 router.get('/:userId/empresa-modulos', listarModulosDaEmpresaDoUser);
 
 // Rota para atualizar dados do utilizador (empresa_areacliente, id_tecnico, tipoUser)
-router.put('/:userId/dados-utilizador', authMiddleware, atualizarDadosUtilizador);
+router.put('/:userId/dados-utilizador', authenticateToken, atualizarDadosUtilizador);
 
-router.get('/:userId', authMiddleware, getDadosUtilizador);
+router.get('/:userId', authenticateToken, getDadosUtilizador);
 
 // Rota para remover utilizador
-router.delete('/:userId', authMiddleware, removerUtilizador);
+router.delete('/:userId', authenticateToken, removerUtilizador);
 
 
 
