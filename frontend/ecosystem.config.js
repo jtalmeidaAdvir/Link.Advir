0e31b21970e901446c50aa8b90c2c171def3efec
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-10-15 10:39:43 +0100

    eco

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,9 +1,11 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: "Link.Advir",
-      script: "npx",
-      args: "serve -s web-build -l 8080", // se o export gerar 'dist', troca para 'dist'
+      script: "./node_modules/serve/bin/serve.js", // ou "node_modules/.bin/serve"
+      args: "-s web-build -l 8080",
+      interpreter: "node",
       exec_mode: "fork",
       instances: 1,
       autorestart: true,
@@ -11,7 +13,7 @@ module.exports = {
       max_restarts: 50,
       restart_delay: 5000,
       exp_backoff_restart_delay: 1000,
-      max_memory_restart: "300M",
+      max_memory_restart: "300M",       // agora o PM2 consegue medir a RAM do processo certo
       watch: false,
       env: { NODE_ENV: "production" }
     }
