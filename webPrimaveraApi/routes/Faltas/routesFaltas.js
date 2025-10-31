9df42f556d4581af894c03a92b87d96e59710fbb
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-10-31 10:40:03 +0000

    a

--- webPrimaveraApi/routes/Faltas/routesFaltas.js
+++ webPrimaveraApi/routes/Faltas/routesFaltas.js
@@ -78,6 +78,63 @@ router.get("/GetListaFaltasFuncionario/:codFuncionario", async (req, res) => {
 });
 
 
+router.get("/GetListaFaltasFuncionariosMensal", async (req, res) => {
+    try {
+        const painelAdminToken = req.headers["authorization"]?.split(" ")[1]; // Obtendo o token do cabeçalho
+        if (!painelAdminToken) {
+            return res
+                .status(401)
+                .json({
+                    error: "Token de administrador não encontrado. Faça login novamente.",
+                });
+        }
+
+        const urlempresa = await getEmpresaUrl(req); // Usando a função para obter o urlempresa
+        if (!urlempresa) {
+            return res
+                .status(400)
+                .json({ error: "URL da empresa não fornecida." });
+        }
+        const { codFuncionario } = req.params;
+        const apiUrl = `http://${urlempresa}/WebApi/AlteracoesMensais/GetListaFaltasFuncionariosMensal`; // A URL completa da API
+        console.log("Enviando solicitação para a URL:", apiUrl);
+
+        const response = await axios.get(apiUrl, {
+            headers: {
+                Authorization: `Bearer ${painelAdminToken}`, // Envia o token para a autenticação
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+
+        if (response.status === 200) {
+            const pedidos = response.data; // Obter os pedidos da resposta
+            if (!pedidos || pedidos.length === 0) {
+                return res
+                    .status(404)
+                    .json({ error: "Nenhuma falta encontrada." });
+            }
+
+            return res.status(200).json(pedidos); // Retorna os pedidos encontrados
+        } else {
+            return res
+                .status(400)
+                .json({
+                    error: "Falha ao listar faltas.",
+                    details: response.data.ErrorMessage,
+                });
+        }
+    } catch (error) {
+        console.error("Erro ao listar faltas:", error.message);
+        return res
+            .status(500)
+            .json({
+                error: "Erro inesperado ao listar faltas",
+                details: error.message,
+            });
+    }
+});
+
 router.get("/GetListaTipoFaltas", async (req, res) => {
     try {
         const painelAdminToken = req.headers["authorization"]?.split(" ")[1]; // Obtendo o token do cabeçalho
