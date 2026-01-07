267bad35eb015dc3e5daba952100a2d7d3a51c0a
jtalmeidaAdvir <jtalmeida@advir.pt>
2026-01-07 15:34:21 +0000

    a

--- backend/controllers/bolsaHorasController.js
+++ backend/controllers/bolsaHorasController.js
@@ -1,4 +1,4 @@
-const BolsaHorasAnual = require('../models/bolsaHorasAnual');
+const BolsaHorasAnual = require('../models/BolsaHorasAnual');
 const User = require('../models/user');
 const { Op } = require('sequelize');
 const { sequelize } = require('../config/db');
